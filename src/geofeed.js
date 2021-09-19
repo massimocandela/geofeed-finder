052f3f4a98a4a250a5e4396765e1d396a49bbb8d
Massimo Candela <massimo@us.ntt.net>
2021-09-19 14:24:25 +0200

    split model into specific file

---
+++ src/geofeed.js
@@ -0,0 +1,28 @@
+import validator from "geofeed-validator";
+import ipUtils from "ip-sub";
+
+export default class Geofeed {
+    constructor(inetnum, prefix, country, region, city, zip) {
+        this.inetnum = inetnum.toLowerCase();
+        this.prefix = ipUtils.toPrefix(prefix);
+        this.country = country ? country.toUpperCase() : null;
+        this.region = region ? region.toUpperCase() : null;
+        this.zip = zip || null;
+        this.city = city;
+        this.valid = true;
+    };
+
+    validate = () => {
+        return validator.fromArray([this.prefix, this.country, this.region, this.city, this.zip]);
+    };
+
+    toString = () => {
+        return [
+            this.prefix,
+            this.country || "",
+            this.region || "",
+            this.city || "",
+            this.zip || ""
+        ].join(",");
+    }
+}
