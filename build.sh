92e6ad146cfb0f919365ab0fa3844974dd578b90
Siddharth Mathur <siddharth@getfastah.com>
2022-03-13 22:26:29 +0530

    Compile macos-arm64 executable

--- build.sh
+++ build.sh
@@ -16,5 +16,7 @@ npm run compile
 
 ./node_modules/.bin/pkg ./package.json --targets node14-macos-x64 --output bin/geofeed-finder-macos-x64 --loglevel=error
 
+./node_modules/.bin/pkg ./package.json --targets node14-macos-arm64 --output bin/geofeed-finder-macos-arm64 --loglevel=error
+
 echo "--> Geofeed finder compiled in bin/"
 
