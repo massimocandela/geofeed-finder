f2464e1f36615f8f9a58e6f2c0f9969322e56ee2
Massimo Candela <massimo@us.ntt.net>
2023-02-16 21:53:47 +0100

    updated iso dataset

--- build.sh
+++ build.sh
@@ -10,11 +10,11 @@ npm ci --silent
 
 npm run compile
 
-./node_modules/.bin/pkg ./package.json --targets node14-win-x64 --output bin/geofeed-finder-win-x64 --loglevel=error
+./node_modules/.bin/pkg ./package.json --targets node18-win-x64 --output bin/geofeed-finder-win-x64 --loglevel=error
 
-./node_modules/.bin/pkg ./package.json --targets node14-linux-x64 --output bin/geofeed-finder-linux-x64 --loglevel=error
+./node_modules/.bin/pkg ./package.json --targets node18-linux-x64 --output bin/geofeed-finder-linux-x64 --loglevel=error
 
-./node_modules/.bin/pkg ./package.json --targets node14-macos-x64 --output bin/geofeed-finder-macos-x64 --loglevel=error
+./node_modules/.bin/pkg ./package.json --targets node18-macos-x64 --output bin/geofeed-finder-macos-x64 --loglevel=error
 
 echo "--> Geofeed finder compiled in bin/"
 
