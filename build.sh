31d7a1d5e90adf243fca4d8440224d2f732b67b5
Siddharth 'Sid' Mathur <siddharth@getfastah.com>
2023-07-04 14:09:08 +0530

    Update build.sh for ARM64 on Linux and MacOS
    Added 2 targets for ARM64 running Linux and MacOS - new Apple silicon M1 and M2, and cloud servers using ARM64

--- build.sh
+++ build.sh
@@ -14,7 +14,13 @@ npm run compile
 
 ./node_modules/.bin/pkg ./package.json --targets node18-linux-x64 --output bin/geofeed-finder-linux-x64 --loglevel=error
 
+./node_modules/.bin/pkg ./package.json --targets node18-linux-arm64 --output bin/geofeed-finder-linux-arm64 --loglevel=error
+
 ./node_modules/.bin/pkg ./package.json --targets node18-macos-x64 --output bin/geofeed-finder-macos-x64 --loglevel=error
+
+# The pkg tool considers macos-arm64 to be an experimental target (https://github.com/vercel/pkg). 
+# Pkg uses ad-hoc certification to sign the MacOS binary. Use your own certificate or skip entirely using --no-signature  
+./node_modules/.bin/pkg ./package.json --targets node18-macos-arm64 --output bin/geofeed-finder-macos-arm64 --loglevel=error
 
 echo "--> Geofeed finder compiled in bin/"
 
