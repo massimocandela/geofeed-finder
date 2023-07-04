4bf6040a92086a0fa53f9f3a3b88b08036adca88
Siddharth 'Sid' Mathur <siddharth@getfastah.com>
2023-07-04 14:18:14 +0530

    build.sh - updated instructions for MacOS code signing

--- build.sh
+++ build.sh
@@ -18,9 +18,14 @@ npm run compile
 
 ./node_modules/.bin/pkg ./package.json --targets node18-macos-x64 --output bin/geofeed-finder-macos-x64 --loglevel=error
 
-# The pkg tool considers macos-arm64 to be an experimental target (https://github.com/vercel/pkg). 
-# Pkg uses ad-hoc certification to sign the MacOS binary. Use your own certificate or skip entirely using --no-signature  
-./node_modules/.bin/pkg ./package.json --targets node18-macos-arm64 --output bin/geofeed-finder-macos-arm64 --loglevel=error
+# The pkg tool considers macos-arm64 to be an experimental target (see https://github.com/vercel/pkg). 
+# Due to the mandatory code signing requirement, before the executable is distributed to end users, it must be signed.
+# Otherwise, it will be immediately killed by kernel on launch.
+# An ad-hoc signature is sufficient.
+# To do that, run pkg on a Mac, or transfer the executable to a Mac
+# and run "codesign --sign - <executable>"
+# or (if you use Linux) install "ldid" utility to PATH and then run pkg again 
+# ./node_modules/.bin/pkg ./package.json --targets node18-macos-arm64 --output bin/geofeed-finder-macos-arm64 --loglevel=error
 
 echo "--> Geofeed finder compiled in bin/"
 
