bb0aee224be9d1c4f42c09fd24abac4f8a4f2376
Massimo Candela <massimo@us.ntt.net>
2024-01-07 14:43:07 +0100

    suppress fetch warning (#30)

--- build.sh
+++ build.sh
@@ -10,11 +10,11 @@ npm ci --silent
 
 npm run compile
 
-./node_modules/.bin/pkg ./package.json --targets node18-win-x64 --output bin/geofeed-finder-win-x64 --loglevel=error
+./node_modules/.bin/pkg ./package.json --options "no-warnings,max-old-space-size=2048" --targets node18-win-x64 --output bin/geofeed-finder-win-x64 --loglevel=error
 
-./node_modules/.bin/pkg ./package.json --targets node18-linux-x64 --output bin/geofeed-finder-linux-x64 --loglevel=error
+./node_modules/.bin/pkg ./package.json --options "no-warnings,max-old-space-size=2048" --targets node18-linux-x64 --output bin/geofeed-finder-linux-x64 --loglevel=error
 
-./node_modules/.bin/pkg ./package.json --targets node18-macos-x64 --output bin/geofeed-finder-macos-x64 --loglevel=error
+./node_modules/.bin/pkg ./package.json --options "no-warnings,max-old-space-size=2048" --targets node18-macos-x64 --output bin/geofeed-finder-macos-x64 --loglevel=error
 
 echo "--> Geofeed finder compiled in bin/"
 
