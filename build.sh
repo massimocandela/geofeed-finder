9b1a809e2e0c88f2c7bffcc066d73f1d6db029a1
Massimo Candela <massimo@us.ntt.net>
2025-06-19 17:42:23 +0200

    upgrade to node22, update dependencies to update regions iso codes, npm audit

--- build.sh
+++ build.sh
@@ -10,13 +10,13 @@ npm ci --silent
 
 npm run compile
 
-./node_modules/.bin/pkg ./package.json --options "no-warnings,max-old-space-size=2048" --targets node18-win-x64 --output bin/geofeed-finder-win-x64 --loglevel=error
+./node_modules/.bin/pkg ./package.json --options "no-warnings,max-old-space-size=2048" --targets node22-win-x64 --output bin/geofeed-finder-win-x64 --loglevel=error
 
-./node_modules/.bin/pkg ./package.json --options "no-warnings,max-old-space-size=2048" --targets node18-linux-x64 --output bin/geofeed-finder-linux-x64 --loglevel=error
+./node_modules/.bin/pkg ./package.json --options "no-warnings,max-old-space-size=2048" --targets node22-linux-x64 --output bin/geofeed-finder-linux-x64 --loglevel=error
 
-./node_modules/.bin/pkg ./package.json --options "no-warnings,max-old-space-size=2048" --targets node18-macos-x64 --output bin/geofeed-finder-macos-x64 --loglevel=error
+./node_modules/.bin/pkg ./package.json --options "no-warnings,max-old-space-size=2048" --targets node22-macos-x64 --output bin/geofeed-finder-macos-x64 --loglevel=error
 
-./node_modules/.bin/pkg ./package.json --options "no-warnings,max-old-space-size=2048" --targets node18-macos-arm64 --output bin/geofeed-finder-macos-arm64 --loglevel=error
+./node_modules/.bin/pkg ./package.json --options "no-warnings,max-old-space-size=2048" --targets node22-macos-arm64 --output bin/geofeed-finder-macos-arm64 --loglevel=error
 
 echo "--> Geofeed finder compiled in bin/"
 
